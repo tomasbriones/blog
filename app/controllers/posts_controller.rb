ae8b3abfeeba606793b3f1f086ade24b4f53a816
tomas briones <tomasbriones@gmail.com>
2021-05-31 15:18:13 -0400

    creating custom form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title)
+    params.require(:post).permit(:title, :comment, :url)
   end
   
 
